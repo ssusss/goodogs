d2e4b2684cdfb297c4c2a073c30b85f9a6d36325
kjh425 <k0j4h25@hanmail.net>
2023-07-12 08:48:00 +0900

    일단 커밋

--- src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
+++ src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
@@ -1,5 +1,32 @@
 package com.sk.goodogs.news.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.sk.goodogs.news.model.vo.News;
+
 public class NewsDao {
+	private Properties prop = new Properties();
+
+	public List<News> findAll(Connection conn, String memberId) {
+		List<News> newsList = new ArrayList<>();
+		String sql = prop.getProperty("findAll");
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rset = pstmt.executeQuery();
+		){
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+			
+		
+		return newsList;
+	}
 
 }
