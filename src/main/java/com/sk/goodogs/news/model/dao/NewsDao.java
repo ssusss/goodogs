1f2e26968e403e4752b9fa54cbdbaa68f61582b3
kjh425 <k0j4h25@hanmail.net>
2023-07-12 09:45:44 +0900

    zjalt

--- src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
+++ src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
@@ -1,10 +1,13 @@
 package com.sk.goodogs.news.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -17,6 +20,16 @@ import com.sk.goodogs.news.model.vo.News;
 public class NewsDao {
 	private Properties prop = new Properties();
 
+	public NewsDao() {
+		String filename =
+				NewsDao.class.getResource("/sql/news/news-query.properties").getPath();
+		try {
+			prop.load(new FileReader(filename));
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public List<News> findAll(Connection conn, String memberId) {
 		List<News> newsList = new ArrayList<>();
 		String sql = prop.getProperty("findAll");
@@ -27,13 +40,15 @@ public class NewsDao {
 			ResultSet rset = pstmt.executeQuery();
 			
 			while(rset.next()) {
-				News news = new News();
 				int newsNo = rset.getInt("news_no");
 				String newsTitle = rset.getString("news_title");
 				String newsCategory = rset.getString("news_category");
-//				int 
+				int newsLikeCnt = rset.getInt("news_like_cnt");
+				int newsReadCnt = rset.getInt("news_read_cnt");
+				Date newsConfirmedDate = rset.getDate("news_confirmed_date");
+				News news = new News(newsNo, memberId, newsTitle, newsCategory, newsCategory, null, newsTitle, newsLikeCnt, newsReadCnt, null);
+				newsList.add(news);
 			}
-			
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
