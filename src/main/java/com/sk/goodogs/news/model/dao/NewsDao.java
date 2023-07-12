3ffcf57f54304064b96df70d83a53d1a4bf4dd72
kjh425 <k0j4h25@hanmail.net>
2023-07-12 09:25:00 +0900

    중간 푸시

--- src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
+++ src/main/java/com/sk/goodogs/news/model/dao/NewsDao.java
@@ -10,6 +10,10 @@ import java.util.Properties;
 
 import com.sk.goodogs.news.model.vo.News;
 
+/**
+ * @author 김준한
+ *
+ */
 public class NewsDao {
 	private Properties prop = new Properties();
 
@@ -17,9 +21,19 @@ public class NewsDao {
 		List<News> newsList = new ArrayList<>();
 		String sql = prop.getProperty("findAll");
 		
-		try(PreparedStatement pstmt = conn.prepareStatement(sql);
+		try(
+			PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, memberId);
 			ResultSet rset = pstmt.executeQuery();
-		){
+			
+			while(rset.next()) {
+				News news = new News();
+				int newsNo = rset.getInt("news_no");
+				String newsTitle = rset.getString("news_title");
+				String newsCategory = rset.getString("news_category");
+//				int 
+			}
+			
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
