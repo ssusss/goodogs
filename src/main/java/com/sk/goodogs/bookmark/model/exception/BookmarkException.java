318ecbc803b8acf5f120fa7f2cff7e51ca6daeec
Sookyeong0505 <sookyeong0505@gmail.com>
2023-07-12 10:15:12 +0900

    bookmark.model.exception 클래스 작성

--- src/main/java/com/sk/goodogs/bookmark/model/exception/BookmarkException.java
+++ src/main/java/com/sk/goodogs/bookmark/model/exception/BookmarkException.java
@@ -1,5 +1,29 @@
 package com.sk.goodogs.bookmark.model.exception;
 
-public class BookmarkException {
+/**
+ * @author 전수경
+ *
+ */
+public class BookmarkException extends RuntimeException {
+
+	public BookmarkException() {
+		super();
+	}
+
+	public BookmarkException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public BookmarkException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BookmarkException(String message) {
+		super(message);
+	}
+
+	public BookmarkException(Throwable cause) {
+		super(cause);
+	}
 
 }
