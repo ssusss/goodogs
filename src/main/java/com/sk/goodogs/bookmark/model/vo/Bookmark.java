dcc5cdf6efb409bcb83c87dc3da706ca032c95af
Sookyeong0505 <sookyeong0505@gmail.com>
2023-07-12 10:14:40 +0900

    bookmark.model.vo 작성

--- src/main/java/com/sk/goodogs/bookmark/model/vo/Bookmark.java
+++ src/main/java/com/sk/goodogs/bookmark/model/vo/Bookmark.java
@@ -1,5 +1,65 @@
 package com.sk.goodogs.bookmark.model.vo;
 
-public class Bookmark {
+import java.sql.Date;
 
+/**
+ * @author 전수경
+ *
+ */
+public class Bookmark {
+	private String memberId;
+	private int newsNo;
+	private String newBookmarkedContent;
+	private Date bookmarkDate;
+	
+	public Bookmark() {
+		super();
+	}
+	
+	public Bookmark(String memberId, int newsNo, String newBookmarkedContent, Date bookmarkDate) {
+		super();
+		this.memberId = memberId;
+		this.newsNo = newsNo;
+		this.newBookmarkedContent = newBookmarkedContent;
+		this.bookmarkDate = bookmarkDate;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public int getNewsNo() {
+		return newsNo;
+	}
+
+	public void setNewsNo(int newsNo) {
+		this.newsNo = newsNo;
+	}
+
+	public String getNewBookmarkedContent() {
+		return newBookmarkedContent;
+	}
+
+	public void setNewBookmarkedContent(String newBookmarkedContent) {
+		this.newBookmarkedContent = newBookmarkedContent;
+	}
+
+	public Date getBookmarkDate() {
+		return bookmarkDate;
+	}
+
+	public void setBookmarkDate(Date bookmarkDate) {
+		this.bookmarkDate = bookmarkDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Bookmark [memberId=" + memberId + ", newsNo=" + newsNo + ", newBookmarkedContent="
+				+ newBookmarkedContent + ", bookmarkDate=" + bookmarkDate + "]";
+	}
+	
 }
