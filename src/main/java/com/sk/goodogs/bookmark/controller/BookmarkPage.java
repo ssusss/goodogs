06b61ddda401896e02047734da04c714e2624c4a
Sookyeong0505 <sookyeong0505@gmail.com>
2023-07-17 15:46:47 +0900

    Bookmark 구현

--- src/main/java/com/sk/goodogs/bookmark/controller/BookmarkPage.java
+++ src/main/java/com/sk/goodogs/bookmark/controller/BookmarkPage.java
@@ -1,6 +1,8 @@
 package com.sk.goodogs.bookmark.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.sk.goodogs.bookmark.model.service.BookmarkService;
+import com.sk.goodogs.bookmark.model.vo.Bookmark;
 import com.sk.goodogs.member.model.vo.Member;
 
 // do get VS do POST 차이
@@ -16,12 +20,13 @@ import com.sk.goodogs.member.model.vo.Member;
 // post : dml (인서트/업데이트/딜리트)
 
 /***
- * @author 이혜령
+ * @author 이혜령, 전수경
  * 북마크 페이지 조회
  */
 @WebServlet("/bookmark/bookmarkPage")
 public class BookmarkPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final BookmarkService bookmarkService = new BookmarkService();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
@@ -31,9 +36,12 @@ public class BookmarkPage extends HttpServlet {
 		String memberId = loginMember.getMemberId();
 		
 		// 북마크 리스트
-	
+		// 1) memberId로 회원의 북마크 조회
+		List<Bookmark> bookmarks = (List<Bookmark>) bookmarkService.findBookmarksByMemberId(memberId);
+		// 2) newsNo로 뉴스 조회 및 세팅
 		
 		
+		request.setAttribute("bookmarks", bookmarks);
 		request.getRequestDispatcher("/WEB-INF/views/member/bookMark.jsp").forward(request, response);
 	}
 
--- src/main/java/com/sk/goodogs/bookmark/model/dao/BookmarkDao.java
+++ src/main/java/com/sk/goodogs/bookmark/model/dao/BookmarkDao.java
@@ -1,5 +1,55 @@
 package com.sk.goodogs.bookmark.model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.sk.goodogs.bookmark.model.exception.BookmarkException;
+import com.sk.goodogs.bookmark.model.vo.Bookmark;
+
+/**
+ * @author 전수경
+ *
+ */
 public class BookmarkDao {
+	private Properties prop = new Properties();
+
+	/**
+	 * @author 전수경
+	 */
+	public List<Bookmark> findBookmarksByMemberId(Connection conn, String memberId) {
+		List<Bookmark> bookmarks = new ArrayList<>();
+		Bookmark bookmark = null;
+		String sql = prop.getProperty("findBookmarksByMemberId");
+		// select * from bookmark where member_id = ?
+		
+		try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+			pstmt.setString(1, memberId);
+			try(ResultSet rset = pstmt.executeQuery()){
+				while(rset.next()) {
+					bookmark = handleBookmarkResultSet(rset);
+					bookmarks.add(bookmark);
+				}
+			}
+			
+		} catch (SQLException e) {
+			throw new BookmarkException(e);
+		}
+		return bookmarks;
+	}
+
+	private Bookmark handleBookmarkResultSet(ResultSet rset) throws SQLException {
+		// Bookmark(memberId, newsNo, newBookmarkedContent, bookmarkDate)
+		String memberId = rset.getString("member_id");
+		int newsNo = rset.getInt("news_no");
+		String newBookmarkedContent = rset.getString("new_bookmarked_content");
+		Timestamp bookmarkDate = rset.getTimestamp("bookmark_date");
+		return new Bookmark(memberId, newsNo, newBookmarkedContent, bookmarkDate);
+	}
 
 }
